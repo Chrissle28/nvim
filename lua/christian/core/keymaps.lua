c9f9301746120b9b872921f9cb1b7906f9bfc037
Christian Zellier <69738385+Chrissle28@users.noreply.github.com>
2024-02-14 17:20:38 +0100

    add renaming keymap

--- lua/christian/core/keymaps.lua
+++ lua/christian/core/keymaps.lua
@@ -23,6 +23,8 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to previous tab
 
+keymap.set("n", "<leader>rn", "vim.lsp.buf.rename()<CR>") -- rename word under cursor
+
 -- plugin keymaps
 
 -- vim-maximizer
@@ -33,10 +35,11 @@ keymap.set("n", "<leader>e", ":NvimTreeToggle")
 
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
+keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
-keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
+keymap.set("n", "<leader>fh", "<cmd>Telescope oldfiles<cr>")
+keymap.set("n", "<leader>fm", "<cmd>Telescope marks<cr>")
 
 -- toggleterm
 keymap.set("n", "<leader>j", "<cmd>ToggleTerm size=15 direction=horizontal<cr>")
